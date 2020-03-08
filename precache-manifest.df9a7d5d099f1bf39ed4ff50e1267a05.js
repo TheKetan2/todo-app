136688f2094946cbd857c0ad090d48f493fd9632
Ketan <ketantoo@gmail.com>
2020-03-08 17:05:05 +0530

    Updates

---
+++ precache-manifest.df9a7d5d099f1bf39ed4ff50e1267a05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dce1b146579ccaed40f3c1b0bc8af46",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "569b0266ef3a3cc9e57a",
+    "url": "/todo-app/static/css/2.eaa14dea.chunk.css"
+  },
+  {
+    "revision": "07b55f6f6179231ab515",
+    "url": "/todo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "569b0266ef3a3cc9e57a",
+    "url": "/todo-app/static/js/2.a688344b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-app/static/js/2.a688344b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b55f6f6179231ab515",
+    "url": "/todo-app/static/js/main.3b01cc72.chunk.js"
+  },
+  {
+    "revision": "c339d801fd14056574f5",
+    "url": "/todo-app/static/js/runtime-main.5f90965c.js"
+  }
+]);
