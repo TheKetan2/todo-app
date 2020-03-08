5c9431ca36ab6a12678a89a50d6918090cf92cb7
Ketan <ketantoo@gmail.com>
2020-03-08 15:14:49 +0530

    Added text input for new todo's

--- src/App.js
+++ src/App.js
@@ -7,15 +7,56 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "Adam"
+      userName: "Adam",
+      todoItems: [
+        { action: "Buy Flowers", done: false },
+        { action: "Buy Car", done: false },
+        { action: "Buy Plane", done: true },
+        { action: "Buy Joe", done: false }
+      ],
+      newItemText: ""
     };
   }
+  updateNewTextValue = event => {
+    this.setState({ newItemText: event.target.value });
+  };
+
+  createNewTodo = () => {
+    if (
+      !this.state.todoItems.find(item => item.action === this.state.newItemText)
+    ) {
+      this.setState({
+        todoItems: [
+          ...this.state.todoItems,
+          { action: this.state.newItemText, done: false }
+        ],
+        newItemText: ""
+      });
+    }
+  };
+
   render() {
     return (
       <div>
         <h4 className="bg-primary text-white text-center p-2">
-          {this.state.userName}'s To Do List
+          {this.state.userName}'s To Do List (
+          {this.state.todoItems.filter(t => !t.done).length} items to do)
         </h4>
+        <div className="container-fluid">
+          <div className="my-1">
+            <input
+              className="form-control"
+              value={this.state.newItemText}
+              onChange={this.updateNewTextValue}
+            />
+            <button
+              className="btn btn-primary mt-1"
+              onClick={this.createNewTodo}
+            >
+              Add
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
